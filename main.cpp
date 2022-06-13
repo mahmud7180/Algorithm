47e96e6c6c4d844eab8a451b59ab8be1a5e0bc52
mahmud7180 <107356365+mahmud7180@users.noreply.github.com>
2022-06-13 22:21:08 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+   int num[]={10,20,80,30,60,50,110,100,130,170};
+   cin>>x;
+   for(int i=0;i<10;i++){
+        if(i==x){
+    cout<<"found"<<num[10]<<endl;
+        }
+   }
+}
